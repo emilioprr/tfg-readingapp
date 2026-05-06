947aec8bb15d573188b7717571114f6a8640890b
emilioprr <emilio.pulpeiro@udc.es>
2026-05-06 05:03:39 +0200

    Commit inicial con el esqueleto generado a partir de Spring initializr

---
+++ src/main/java/com/readingapp/reading_app/ReadingAppApplication.java
@@ -0,0 +1,13 @@
+package com.readingapp.reading_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readingapp/reading_app/ReadingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readingapp.reading_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
