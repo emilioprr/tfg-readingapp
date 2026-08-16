daeec911db10527159c179804071183b93602596
emilioprr <emilio.pulpeiro@udc.es>
2026-08-16 13:47:11 +0200

    Correccion respecto a la paginacion

--- src/main/java/com/readingapp/reading_app/repository/NotificacionRepository.java
+++ src/main/java/com/readingapp/reading_app/repository/NotificacionRepository.java
@@ -2,6 +2,7 @@ package com.readingapp.reading_app.repository;
 
 import com.readingapp.reading_app.model.Notificacion;
 import com.readingapp.reading_app.model.enums.TipoNotificacion;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,8 +10,9 @@ import java.util.List;
 
 @Repository
 public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
-    List<Notificacion> findByUsuarioIdusuarioOrderByFechaDesc(Long idusuario,  Pageable pageable);
-    List<Notificacion> findByUsuarioIdusuarioAndLeidaFalseOrderByFechaDesc(Long idusuario, Pageable pageable);
-    List<Notificacion> findByUsuarioIdusuarioAndTipoOrderByFechaDesc(Long idusuario, TipoNotificacion tipo,  Pageable pageable);
+    Page<Notificacion> findByUsuarioIdusuarioOrderByFechaDesc(Long idusuario, Pageable pageable);
+    Page<Notificacion> findByUsuarioIdusuarioAndLeidaFalseOrderByFechaDesc(Long idusuario, Pageable pageable);
+    List<Notificacion> findByUsuarioIdusuarioAndLeidaFalseOrderByFechaDesc(Long idusuario);
+    Page<Notificacion> findByUsuarioIdusuarioAndTipoOrderByFechaDesc(Long idusuario, TipoNotificacion tipo, Pageable pageable);
     long countByUsuarioIdusuarioAndLeidaFalse(Long idusuario);
 }
