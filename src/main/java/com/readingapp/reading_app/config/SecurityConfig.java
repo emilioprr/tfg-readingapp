c3cfaf1ab09badc4867bde98c819267d01bb7545
emilioprr <emilio.pulpeiro@udc.es>
2026-09-08 02:41:09 +0200

    ajuste para el deploy

--- src/main/java/com/readingapp/reading_app/config/SecurityConfig.java
+++ src/main/java/com/readingapp/reading_app/config/SecurityConfig.java
@@ -57,7 +57,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://bookmark-tau-ten.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:3000",
+                "http://localhost:5173",
+                "https://bookmark-tau-ten.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
